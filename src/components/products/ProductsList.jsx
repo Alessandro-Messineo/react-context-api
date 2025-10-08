950be01583d4b81677043e0b1501e51e1c877b0a
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-10-08 17:25:56 +0200

    milestone 3

--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import ProductsCard from "./ProductsCard";
+import { useBudgetMode } from "../../context/BudgetContext";
 
 export default function ProductsList() {
 
     const [products, setProducts] = useState([]);
+
+    const { budgetMode } = useBudgetMode();
 
     function fetchProducts() {
         axios.get("https://fakestoreapi.com/products")
@@ -16,9 +19,13 @@ export default function ProductsList() {
         fetchProducts()
     }, [])
 
+     const filteredProducts = (budgetMode
+        ? products.filter(product => product.price <= 30)
+        : products);
+
     return (
         <div className="container-card">
-            {products.map((product, index) => (
+            {filteredProducts.map((product, index) => (
                 <ProductsCard 
                 key={index}
                 // image = {product.image}
